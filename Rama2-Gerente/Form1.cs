481c1b0135e637ce68367b86903afef6047bb9af
riascas <ruth.iascas@gmail.com>
2021-06-06 23:36:09 -0300

    Form Administración de alertas - Gerente

--- Rama2-Gerente/Form1.cs
+++ Rama2-Gerente/Form1.cs
@@ -17,5 +17,39 @@ namespace Rama2_Gerente
             InitializeComponent();
         }
 
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MaskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
     }
 }
